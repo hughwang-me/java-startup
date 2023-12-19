84b6da7a631400fa3c9fc5c15e4bf22470854967
wanghuan <18501667737@163.com>
2023-12-19 16:48:54 +0800

    初始化

---
+++ src/main/java/com/uwjx/javastartup/JavaStartupApplication.java
@@ -0,0 +1,13 @@
+package com.uwjx.javastartup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStartupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaStartupApplication.class, args);
+    }
+
+}
